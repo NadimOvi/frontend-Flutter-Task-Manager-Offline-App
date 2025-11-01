580a23ef1d3108941510a2d2a4b5bdf4b663c77d
NadimOvi <nadimovi20@gmail.com>
2025-11-01 03:43:12 +0100

    Sign Up page UI

--- lib/features/auth/pages/signup_page.dart
+++ lib/features/auth/pages/signup_page.dart
@@ -8,8 +8,27 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Sign up Page')));
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Sign up.',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            TextFormField(),
+          ],
+        ),
+      ),
+    );
   }
 }
